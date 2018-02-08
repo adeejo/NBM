1aefd0328a7e093b98d9f07bb4aa68338403f749
adeejo <adeejo1@gmail.com>
2018-02-08 07:25:02 -0500

    column change for gearboxtemp

--- LSTM_v5.py
+++ LSTM_v5.py
@@ -35,25 +35,30 @@ from pandas import read_csv
 from matplotlib import pyplot
 from keras.layers import Masking
 
-dataset = read_csv('C:\\Users\\Admin\\Documents\\SCADA\\all_var_1000_noOut.txt', header=0, index_col=0, sep="\t")
+dataset = read_csv('C:\\Users\\Admin\\Documents\\SCADA\\all_var_1000_v5.txt', header=0, index_col=0, sep="\t")
+gear_temp = read_csv('C:\\Users\\Admin\\Documents\\SCADA\\gear.txt', header=0, index_col=0, sep="\t")
 n_train_hours = 700
 values = dataset.values
-#values[values < 0] = -1 # make all negative values -1
+gear = gear_temp.values
+gear_train = gear[:n_train_hours]
+gear_test = gear[n_train_hours:]
+
 
 train = values[:n_train_hours, :]
 test = values[n_train_hours:, :]
-train_X, train_y = train[:, 2:], train[:, :1]#output variable is the first column
-train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1])) #reshape the data to have input,timestamp, outputshape
+train_X, train_y = train[:, :-1], train[:, -1]
+test_X, test_y = test[:, :-1], test[:, -1] #reshape the data to have input,timestamp, outputshape
    
+gear_test = gear_test.reshape((gear_test.shape[0], 1, gear_test.shape[1]))
 
 
-test_X, test_y = test[:, 2:], test[:, :1]
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
 test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
 
 model = Sequential()
-model.add(Masking(mask_value=-4, input_shape=(train_X.shape[1], 10) ))
+#model.add(Masking(mask_value=-4, input_shape=(train_X.shape[0], 11) ))
 
-model.add(LSTM(50, input_shape=(train_X.shape[1], 10),return_sequences = True)) # Number sample/alpha(Number of )
+model.add(LSTM(50, input_shape=(train_X.shape[1],train_X.shape[2] ),return_sequences = True)) # Number sample/alpha(Number of )
 model.add(LSTM(25,return_sequences = True))
 model.add(LSTM(10))
 
@@ -88,14 +93,17 @@ print('Test RMSE: %.3f' % rmse)
 
 rmses = sqrt(mean_squared_error(invt_y, xy))
 print('Train RMSE: %.3f' % rmses)
-plt.ylim( 50, 75 ) 
-pyplot.plot(yhat*4.976601+62.6154191,color="blue",label ="Predicted")
-pyplot.plot(inv_y*4.976601+62.6154191,color="red",label ="Actual")
+pyplot.ylim( 50, 75 ) 
+#test_gear=yhat+(gear_test)
+#actual_gear_test= inv_y+(gear_test)
+
+pyplot.plot((yhat*4.730348145)+62.4926759,color="blue",label ="Predicted")
+pyplot.plot((inv_y*4.730348145)+62.4926759,color="red",label ="Actual")
 pyplot.legend()
 pyplot.show()
-plt.ylim( 50, 75 ) 
-pyplot.plot(xy*4.976601+62.6154191,color ="blue",label="Predicted")
-pyplot.plot(train_y*4.976601+62.6154191,color="red",label ="Actual")
+#pyplot.ylim( 50, 75 ) 
+pyplot.plot((xy*4.730348145)+62.4926759,color ="blue",label="Predicted")
+pyplot.plot((invt_y*4.730348145)+62.4926759,color="red",label ="Actual")
 pyplot.legend()
 pyplot.show()
 
